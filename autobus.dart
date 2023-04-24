46ba649ec6092b8dc8527800953efc0b46be42ab
Pavel Kryl <pavel@kryl.eu>
2023-04-24 11:23:17 +0200

    autobus

---
+++ autobus.dart
@@ -0,0 +1,19 @@
+
+class Ridic {
+  String name;
+
+  Ridic({required this.name});
+
+}
+
+class Autobus {
+  String nazev;
+  Ridic ridic;
+  List<String> pasazeri = [];
+
+  Autobus({required this.nazev, required this.ridic});
+
+}
+
+void main() {
+}
